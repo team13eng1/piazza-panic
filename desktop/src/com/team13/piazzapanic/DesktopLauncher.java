4e5beffe5d719d0778a622b37375a26231622a81
Elizabeth <30590826+lizzy02g@users.noreply.github.com>
2022-12-04 08:40:34 +0000

    Setup libGDX

---
+++ desktop/src/com/team13/piazzapanic/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.team13.piazzapanic;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.team13.piazzapanic.MainGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PiazzaPanic");
+		new Lwjgl3Application(new MainGame(), config);
+	}
+}
