d48cd2f78bc681c704cd0bcba6dd5067b426eb1d
Alex <am3211@york.ac.uk>
2023-01-31 00:04:29 +0000

    done docstrings now jeez

--- core/src/Sprites/Worktop.java
+++ core/src/Sprites/Worktop.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
+/**
+ * Represents a Worktop in the game in which blocks the chefs movement around the kitchen to stop him from
+ * escaping the bounds
+ */
 public class Worktop extends InteractiveTileObject {
     public Worktop(World world, TiledMap map, BodyDef bdef, Rectangle rectangle) {
         super(world, map, bdef, rectangle);
